9ffc7c8c5c96b75e550b512d3f4e6c28d8016cf6
Jonathan <Jonathan>
2021-07-25 18:54:29 +0100

    Twitter scraper for scraping tweets with certain keywords

---
+++ Data/Scrape.py
@@ -0,0 +1,42 @@
+import time
+from datetime import datetime
+import pandas as pd
+import numpy as np
+import twint
+
+def scrape(start_date: str, end_date: str, keywords: str):
+    config = twint.Config()
+    config.Search = keywords
+    config.Lang = "en"
+    config.Min_likes = 50
+    config.Since = start_date
+    config.Until = end_date
+    # config.Limit = 100 for testing
+
+    config.Hide_output = True
+    config.Store_csv = True
+    config.Output = f"Tweets/bitcoin_{start_date}_{end_date}.csv"
+
+    temp_time = time.time()
+    twint.run.Search(config)
+    runtime_seconds = time.time() - temp_time
+    runtime_minutes = np.round(runtime_seconds / 60, 1)
+    print(f"Downloaded the data in {runtime_minutes} minutes")
+    return float(runtime_minutes)
+
+# Set time period and keywords
+START_DATE = datetime(2018, 1, 1)
+END_DATE = datetime(2021, 6, 30)
+KEYWORDS = 'bitcoin'
+
+dtrange = pd.date_range(start=START_DATE, end=END_DATE, freq='d')
+months = pd.Series(dtrange.month)
+starts, ends = months.ne(months.shift(1)), months.ne(months.shift(-1))
+df = pd.DataFrame({'m_start': dtrange[starts].strftime('%Y-%m-%d'), 'm_end': dtrange[ends].strftime('%Y-%m-%d')})
+
+time_list = []
+for idx in range(len(df)):
+    start_date = df['m_start'][idx]
+    end_date = df['m_end'][idx]
+    time_list.append(scrape(start_date,end_date,KEYWORDS))
+print(time_list)
