56e21279180c76d517f6e1bb0b41bd62d0a2e6ff
Jonathan <Jonathan>
2021-08-01 14:41:36 +0100

    Updated Scraping to include the missing data

--- Data/Scrape.py
+++ Data/Scrape.py
@@ -8,14 +8,14 @@ def scrape(start_date: str, end_date: str, keywords: str):
     config = twint.Config()
     config.Search = keywords
     config.Lang = "en"
-    config.Min_likes = 50
+    config.Min_likes = 3
     config.Since = start_date
     config.Until = end_date
     # config.Limit = 100 for testing
 
     config.Hide_output = True
     config.Store_csv = True
-    config.Output = f"Tweets/bitcoin_{start_date}_{end_date}.csv"
+    config.Output = f"Tweets/{keywords}_{start_date}_{end_date}.csv"
 
     temp_time = time.time()
     twint.run.Search(config)
@@ -26,7 +26,7 @@ def scrape(start_date: str, end_date: str, keywords: str):
 
 # Set time period and keywords
 START_DATE = datetime(2018, 1, 1)
-END_DATE = datetime(2021, 6, 30)
+END_DATE = datetime(2021, 7, 1)
 KEYWORDS = 'bitcoin'
 
 dtrange = pd.date_range(start=START_DATE, end=END_DATE, freq='d')
@@ -35,8 +35,8 @@ starts, ends = months.ne(months.shift(1)), months.ne(months.shift(-1))
 df = pd.DataFrame({'m_start': dtrange[starts].strftime('%Y-%m-%d'), 'm_end': dtrange[ends].strftime('%Y-%m-%d')})
 
 time_list = []
-for idx in range(len(df)):
+for idx in range(len(df)-1):
     start_date = df['m_start'][idx]
-    end_date = df['m_end'][idx]
+    end_date = df['m_start'][idx+1]
     time_list.append(scrape(start_date,end_date,KEYWORDS))
 print(time_list)
